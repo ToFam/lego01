caaca2806ddb2824823eacdc07491bd729bb1fa9
SimonBlasen <simon.yzf@t-online.de>
2017-01-30 15:14:55 +0100

    Consistent commit

--- src/remoteTest/Main.java
+++ src/remoteTest/Main.java
@@ -1,24 +1,95 @@
 package remoteTest;
 
 import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.hardware.motor.Motor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
-import lejos.internal.ev3.EV3MotorPort;
-import lejos.robotics.RegulatedMotor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
 
 public class Main {
 
     static EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
     static EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+    static EV3MediumRegulatedMotor mediumMotor = new EV3MediumRegulatedMotor(MotorPort.C);
+    static EV3UltrasonicSensor ultraSens = new EV3UltrasonicSensor(SensorPort.S1);
+    
+    static boolean ultraLooksDown = false;
     
 	public static void main(String[] args)
 	{
-		while ((Button.readButtons() & Button.ID_ESCAPE) == 0)
+		leftMotor.setSpeed(leftMotor.getMaxSpeed() * 0.5f);
+		rightMotor.setSpeed(rightMotor.getMaxSpeed() * 0.5f);
+		mediumMotor.setSpeed(mediumMotor.getMaxSpeed() * 1.0f);
+
+		LCD.drawString("Ultra: ", 0,0);
+		
+		while (cancelProgram() == false)
 		{
-			leftMotor.rotate(360);
-			rightMotor.rotate(360);
+			SampleProvider sP = ultraSens.getDistanceMode();
+			float[] samples = new float[sP.sampleSize()];
+			sP.fetchSample(samples, 0);
+			float mid = 0f;
+			for (int i = 0; i < samples.length; i++)
+			{
+				mid += samples[i];
+			}
+			mid /= samples.length;
+			LCD.drawString(String.valueOf(mid), 7, 0);
+			
+			
+			if (buttonForwardPressed())
+			{
+				leftMotor.backward();
+				rightMotor.backward();
+			}
+			
+			if (buttonEnterPressed())
+			{
+				leftMotor.stop();
+				rightMotor.stop();
+			}
+			
+			if (buttonPressed(Button.ID_LEFT))
+			{
+				leftMotor.forward();
+				rightMotor.backward();
+			}
+			
+			if (buttonPressed(Button.ID_RIGHT))
+			{
+				leftMotor.backward();
+				rightMotor.forward();
+			}
+			
+			if (buttonPressed(Button.ID_DOWN))
+			{
+				ultraLooksDown = !ultraLooksDown;
+				mediumMotor.rotate(ultraLooksDown ? 90 : -90);
+			}
 		}
+	}
+	
+	private static boolean cancelProgram()
+	{
+		return (Button.readButtons() & Button.ID_ESCAPE) != 0;
+	}
+	
+	private static boolean buttonForwardPressed()
+	{
+		return (Button.readButtons() & Button.ID_UP) != 0;
+	}
+	
+	private static boolean buttonEnterPressed()
+	{
+		return (Button.readButtons() & Button.ID_ENTER) != 0;
+	}
+	
+	private static boolean buttonPressed(int button)
+	{
+		return (Button.readButtons() & button) != 0;
 	}
 
 }
