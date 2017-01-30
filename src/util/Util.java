a93a905ae645f91557b169f53178e79a6e78a204
AdrianHeyl <uudlq@student.kit.edu>
2017-01-30 15:07:47 +0100

    Util um isRuning() erweitert.

--- src/util/Util.java
+++ src/util/Util.java
@@ -27,4 +27,14 @@ public final class Util {
 	public static boolean isPressed(int buttonID) {
 		return (Button.readButtons() & buttonID) == 0;
 	}
+	
+	/**
+	 * Returns the run condition for an infinite while loop.
+	 * This method uses isPressed(Button.ID_ESCAPE) to determine the run condition.
+	 * 
+	 * @return Whether to continue iterating an infinite while loop.
+	 */
+	public static boolean isRunning() {
+		return isPressed(Button.ID_ESCAPE);
+	}
 }
