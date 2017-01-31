cbf5447c56046dd4eddfcd22eb5e9f4c4e7e37e3
SimonBlasen <simon.yzf@t-online.de>
2017-01-31 15:24:53 +0100

    Added System.exit(0) on pressing escape-key

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
 import lcdGui.LCDGui;
+import lejos.hardware.Button;
+import lejos.hardware.Key;
+import lejos.hardware.KeyListener;
 import lejos.hardware.lcd.LCD;
 import lejos.robotics.SampleProvider;
 import robot.Robot;
@@ -67,6 +70,18 @@ public class Main {
     
 	public static void main(String[] args)
 	{
+		Button.ESCAPE.addKeyListener(new KeyListener() {
+			@Override
+			public void keyReleased(Key k) {
+				// TODO Auto-generated method stub
+				
+			}
+			@Override
+			public void keyPressed(Key k) {
+				System.exit(0);
+			}
+		});
+		
 	    Main m = new Main();
 	    m.run();
 	}
