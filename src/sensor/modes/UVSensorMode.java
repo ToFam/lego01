45de9d847218414e5c2cf3b750cf7af35c1d12c6
SimonBlasen <simon.yzf@t-online.de>
2017-02-01 23:22:11 +0100

    Added mode enum for uv sensor. Added Test for driving left to a wall

--- src/sensor/modes/UVSensorMode.java
+++ src/sensor/modes/UVSensorMode.java
@@ -1,11 +1,10 @@
 package sensor.modes;
 
-
-public enum GyroSensorMode {
-	ANGLE(1), ANGLE_SPEED(0), BOTH(2);
+public enum UVSensorMode {
+	DISTANCE(0), ONLY_LISTEN(1);
 	
 	private int identifier;
-	private GyroSensorMode(int c0)
+	private UVSensorMode(int c0)
 	{
 		this.identifier = c0;
 	}
---
+++ src/state/TestDriveLTWall.java
@@ -0,0 +1,37 @@
+package state;
+
+import lcdGui.LCDGui;
+import lejos.hardware.Button;
+import robot.Robot;
+import robot.RobotComponents;
+import sensor.modes.UVSensorMode;
+import util.Util;
+
+public class TestDriveLTWall implements ParcourState {
+    
+    private Robot robot;
+    private LCDGui gui;
+    
+    public TestDriveLTWall(Robot robot, LCDGui gui) {
+        this.robot = robot;
+        this.gui = gui;
+    }
+
+    @Override
+    public void init() {
+        robot.setSpeed(1f);
+        RobotComponents.inst().getUV().setMode(UVSensorMode.DISTANCE.getIdf());
+        
+        gui = new LCDGui(4, 2);
+    }
+
+    @Override
+    public void update() {
+
+    	
+    	
+    	gui.setVarValue(0, RobotComponents.inst().getUV().sample()[0]);
+    	
+    	
+    }
+}
