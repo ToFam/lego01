19cd5beced345cd4c9353c9c98a042a5815cc3d4
AdrianHeyl <uudlq@student.kit.edu>
2017-02-06 11:21:42 +0100

    HumpbackBridgeState almost done.

--- src/state/HumpbackBridgeState.java
+++ src/state/HumpbackBridgeState.java
@@ -2,7 +2,10 @@ package state;
 
 import robot.Robot;
 import robot.RobotComponents;
+import sensor.modes.ColorSensorMode;
+import sensor.modes.GyroSensorMode;
 import sensor.modes.UVSensorMode;
+import state.LabyrinthState.State;
 import util.lcdGui.LCDGui;
 
 public class HumpbackBridgeState implements ParcourState {
@@ -10,48 +13,61 @@ public class HumpbackBridgeState implements ParcourState {
 	private final Robot robot;
 	private final LCDGui gui;
 	private final float SPEED_MAX;
+	private final float SPEED_MIN_LEFT;
+	private final float SPEED_MIN_RIGHT;
 	private float SPEED_LEFT;
 	private float SPEED_RIGHT;
 	private final float MINUS_LEFT_DELTA;
 	private final float MINUS_RIGHT_DELTA;
-	private final float PLUS_LEFT_DELTA;
-	private final float PLUS_RIGHT_DELTA;
+	private final float TURN_INCREASE;
+	private float turn_delta;
 	private final float THRESHOLD_NO_GROUND;
-	private final float THRESHOLD_CLIFF;
-	private final int PAST;
-	private int SEGMENT_COUNT;
+	
+	private final int GYRO_ALARM;
+	private float angle_no_cliff;
+	private float[] angles;
+	private int angle_fresh;
+	private int angle_old;
+	private boolean turning;
 	
 	private BridgeSegment bridgeSegment;
 	
-	private float[] heights;
-	private int current;
+	private float distance;
+	private float correction;
+
+	private float color;
 	
 	private enum BridgeSegment {
-		SEGMENTS_NOT_USED,
-		RAMP_UP,
-		PLANK,
-		RAMP_DOWN,
+		PRE_COLLISION,
+		COLLISION,
+		POST_COLLISION,
+		FOLLOW_PLANK,
+		ENTIRE_BRIDGE,
+		TEST_SENSOR,
 	}
 	
 	public HumpbackBridgeState(Robot robot) {
 		this.robot = robot;
-		this.gui = new LCDGui(2, 1);
+		this.gui = new LCDGui(3, 1);
 		this.SPEED_MAX = 1f;
-		this.SPEED_LEFT = this.SPEED_MAX;
-		this.SPEED_RIGHT = this.SPEED_MAX;
-		this.MINUS_LEFT_DELTA = 0.02f;
-		this.MINUS_RIGHT_DELTA = 0.02f;
-		this.PLUS_LEFT_DELTA = 0.05f;
-		this.PLUS_RIGHT_DELTA = 0.05f;
+		this.SPEED_MIN_LEFT = .6f;
+		this.SPEED_MIN_RIGHT = .5f;
+		this.MINUS_LEFT_DELTA = .3f;
+		this.MINUS_RIGHT_DELTA = .5f;
+		this.TURN_INCREASE = 0.05f;
+//		this.TURN_INCREASE = 0.00f;
+		this.turn_delta = 0.f;
 		this.THRESHOLD_NO_GROUND = .04f;
-		this.THRESHOLD_CLIFF = 0.39f;
-		this.PAST = 10;
-		this.SEGMENT_COUNT = 0;
 		
-		this.bridgeSegment = BridgeSegment.RAMP_UP;
+		this.GYRO_ALARM = 3;
+		this.angles = new float[50];
+		this.turning = false;
+		this.correction = 0.f;
 		
-		this.heights = new float[1000];
+		this.color = 0.f;
 	}
+//	timer, der cliff sieht und in liste zur ausgabe/manuelle Abstimmung ausgibt.
+//	Schärfere Linkskurve zulassen.
 	@Override
 	public boolean changeOnBarcode()
 	{
@@ -62,164 +78,174 @@ public class HumpbackBridgeState implements ParcourState {
 	public String getName() {
 		return "HumpbackBridge";
 	}
-
 	@Override
 	public void init() {
-		this.robot.lowerUV();
+		
+        RobotComponents.inst().getGyroSensor().setMode(GyroSensorMode.ANGLE.getIdf());
 		RobotComponents.inst().getUV().setMode(UVSensorMode.DISTANCE.getIdf());
 		RobotComponents.inst().getUV().setMedianFilter(1);
+		RobotComponents.inst().getColorSensor().setMode(ColorSensorMode.RED.getIdf());
 		
-		for (int i = 0; i < this.PAST + 1; i++) {
-			this.heights[0] = RobotComponents.inst().getUV().sample()[0];
+		this.angle_no_cliff = RobotComponents.inst().getGyroSensor().sample()[0];
+		for (int i = 0; i < this.angles.length; i++) {
+			this.angles[i] = this.angle_no_cliff;
 		}
+		this.angle_fresh = 0;
+		this.angle_old = this.angles.length - 1;
+
+		this.SPEED_LEFT = this.SPEED_MAX;
+		this.SPEED_RIGHT = this.SPEED_MAX;
+		this.turn_delta = -this.TURN_INCREASE;
 		
-		this.current = this.PAST;
-		this.bridgeSegment = BridgeSegment.SEGMENTS_NOT_USED;
+//		this.bridgeSegment = BridgeSegment.PRE_COLLISION;
+		this.bridgeSegment = BridgeSegment.ENTIRE_BRIDGE;
+		this.robot.lowerUV();
+//		this.bridgeSegment = BridgeSegment.TEST_SENSOR;
 		
-		while (!this.robot.UVisDown()) {}
+		this.robot.setSpeed(this.SPEED_MAX);
+		this.robot.forward();
+		
 	}
 
 	@Override
 	public void update(int elapsedTime) {
-		do {
-		this.heights[this.current] = RobotComponents.inst().getUV().sample()[0];
-		} while (this.heights[this.current] == Float.POSITIVE_INFINITY);
-//		this.current++;
-		gui.setVarValue(0, this.bridgeSegment.toString());
-//		gui.setVarValue(0, String.valueOf(current));
-		gui.setVarValue(1, String.valueOf(this.heights[this.current]), 5);
 		
-		switch (this.bridgeSegment) {
-		case SEGMENTS_NOT_USED:
-			
-			if (this.heights[this.current] > this.THRESHOLD_NO_GROUND) {
-				
-				this.SPEED_LEFT = this.SPEED_MAX;
-				this.slowDownRightMotor();
-			
-			} else {
-				
-				this.SPEED_RIGHT = this.SPEED_MAX;
-				this.slowDownLeftMotor();
-				
-			}
+		this.angle_fresh = (this.angle_fresh + 1) % this.angles.length;
+		this.angle_old = (this.angle_old + 1) % this.angles.length;
+		this.angles[angle_fresh] = RobotComponents.inst().getGyroSensor().sample()[0];
 		
-			break;
+		if (turning) {
 			
-		case RAMP_UP:
-			
-			if (this.heights[(this.current + this.heights.length - this.PAST) % this.heights.length] > this.THRESHOLD_CLIFF
-					&& this.heights[(this.current + this.heights.length - this.PAST) % this.heights.length] == this.heights[this.current]) {
-				
-				if (this.SEGMENT_COUNT == 10) {
-					
-					this.bridgeSegment = BridgeSegment.PLANK;
-					this.SEGMENT_COUNT = 0;
-					
-				} else {
-					
-					this.SEGMENT_COUNT++;
-					
-				}
-				
-			} else if (this.heights[(this.current + this.heights.length - this.PAST) % this.heights.length] > this.heights[this.current]) {
-					
-				if (this.SEGMENT_COUNT == 10) {
-					
-					this.bridgeSegment = BridgeSegment.RAMP_DOWN;
-					this.SEGMENT_COUNT = 0;
-					
-				} else {
-					
-					this.SEGMENT_COUNT++;
-				}
-					
-			} else {
-				this.SEGMENT_COUNT = 0;
+			if (!this.robot.finished()) {
+				return;
 			}
 			
-			if (this.heights[this.current] > this.THRESHOLD_NO_GROUND) {
-				
-				this.SPEED_LEFT = this.SPEED_MAX;
-				this.slowDownRightMotor();
-			
-			} else {
-				
-				this.SPEED_RIGHT = this.SPEED_MAX;
-				this.slowDownLeftMotor();
-				
-			}
-		
-			break;
-			
-		case PLANK:
-			
-			if (this.heights[(this.current + this.heights.length - this.PAST) % this.heights.length] < this.THRESHOLD_CLIFF
-//					&& this.heights[(this.current + this.heights.length - this.PAST) % this.heights.length] == this.heights[this.current]
-							) {
-				
-				if (this.SEGMENT_COUNT == 10) {
-					
-					this.bridgeSegment = BridgeSegment.RAMP_DOWN;
-					this.SEGMENT_COUNT = 0;
-					
-				} else {
-					
-					this.SEGMENT_COUNT++;
-				}
-				
-			} else {
-				this.SEGMENT_COUNT = 0;
+			float temp = RobotComponents.inst().getGyroSensor().sample()[0];
+			for (int i = 0; i < this.angles.length; i++) {
+				this.angles[i] = temp;
 			}
 			
-			if (this.heights[this.current] > this.THRESHOLD_NO_GROUND) {
-					
-				this.speedUpLeftMotor();
-				this.slowDownRightMotor();
+			turning = false;
 			
-			} else {
-				
-				this.SPEED_RIGHT = this.SPEED_MAX;
-				this.slowDownLeftMotor();
-				
-			}
-			
-			
-			break;
-		case RAMP_DOWN:
-			
-			if (this.heights[(this.current + this.heights.length - this.PAST) % this.heights.length] == this.heights[this.current]) {
-				
-				if (this.SEGMENT_COUNT == 10) {
-					
-					this.SPEED_RIGHT = this.SPEED_MAX;
-					this.robot.setSpeed(this.SPEED_MAX);
-					this.robot.forward();
-					
-				} else {
-					
-					this.SEGMENT_COUNT++;
-				
-				}
-				
-			}
-			
-			if (this.heights[this.current] > this.THRESHOLD_NO_GROUND) {
-				
-				this.SPEED_LEFT = this.SPEED_MAX;
-				this.slowDownRightMotor();
-			
-			} else {
-				
-				this.SPEED_RIGHT = this.SPEED_MAX;
-				this.slowDownLeftMotor();
-				
-			}
-		
-			break;
 		}
 		
-		this.current = (this.current + 1) % this.heights.length;
+		switch (this.bridgeSegment) {
+		case PRE_COLLISION:
+			
+			if (RobotComponents.inst().getTouchSensorB().sample()[0] == 1) 
+            {
+                this.robot.stop();
+                this.robot.setSpeed(this.SPEED_MAX);
+                this.robot.move(300);
+                this.bridgeSegment = BridgeSegment.COLLISION;
+            }
+			
+			break;
+			
+		case COLLISION:
+			
+			if (this.robot.finished())
+            {
+                this.robot.turnOnSpot(-80);
+                this.bridgeSegment = BridgeSegment.POST_COLLISION;
+            }
+			
+			break;
+			
+		case POST_COLLISION:
+			
+			if (this.robot.finished())
+			{
+				this.bridgeSegment = BridgeSegment.FOLLOW_PLANK;
+			}
+			
+			break;
+			
+		case FOLLOW_PLANK:
+			
+			do {
+				this.distance = RobotComponents.inst().getUV().sample()[0];
+			} while (this.distance == Float.POSITIVE_INFINITY);
+			
+			if (this.distance > .5f)
+			{
+				this.robot.lowerUV();
+				this.robot.setSpeed(this.SPEED_MAX);
+				this.robot.forward();
+				while (!this.robot.UVisDown()) {}
+				this.bridgeSegment = BridgeSegment.ENTIRE_BRIDGE;
+			}
+			else
+			{
+				this.correction = (this.distance - 0.08f) * 8f;
+                robot.steer(Math.max(-0.8f, Math.min(0.8f, correction)));
+	            robot.forward();
+			}
+			
+			break;
+			
+		case ENTIRE_BRIDGE:
+			
+			this.color = RobotComponents.inst().getColorSensor().sample()[0];
+			
+			if (this.color > 0.8f) {
+				this.robot.stop();
+			}
+			
+			do {
+				this.distance = RobotComponents.inst().getUV().sample()[0];
+			} while (this.distance == Float.POSITIVE_INFINITY);
+			
+			if (Math.abs(this.angles[this.angle_old] - this.angles[this.angle_fresh]) > this.GYRO_ALARM) {
+				
+				this.turning = true;
+				this.gui.setVarValue(0, this.angles[this.angle_old] - this.angles[this.angle_fresh]);
+				this.robot.turnOnSpot(this.angles[this.angle_old] - this.angles[this.angle_fresh]);
+				return;
+				
+			}
+
+			if (this.distance > this.THRESHOLD_NO_GROUND) {
+				
+//				if (!this.cliff) {
+//					
+////					if ((this.angle_no_cliff - this.angles[this.angle_fresh]) > 20) {
+////						RobotComponents.inst().getRightMotor().rotate(100, false);
+////					}
+//					
+//					this.turn_delta = -this.TURN_INCREASE;
+//					this.SPEED_LEFT = this.SPEED_MAX;
+//					this.cliff = true;
+//				}
+				
+				this.SPEED_LEFT = this.SPEED_MAX;
+				this.slowDownRightMotor();
+			
+			} else {
+				
+//				if (this.cliff) {
+//
+//					this.turn_delta = -this.TURN_INCREASE;
+//					this.angle_no_cliff = this.angles[this.angle_fresh];
+//					this.SPEED_RIGHT = this.SPEED_MAX;
+//					this.cliff = false;
+//					
+//				}
+				
+				this.SPEED_RIGHT = this.SPEED_MAX;
+				this.slowDownLeftMotor();
+				
+			}
+		
+			break;
+			
+		case TEST_SENSOR:
+			
+			this.robot.stop();
+			gui.setVarValue(0, color);
+			
+			break;
+		}
 	}
 
 	@Override
@@ -230,26 +256,16 @@ public class HumpbackBridgeState implements ParcourState {
 		while (!this.robot.UVisUp()) {}
 	}
 	
-	private void speedUpLeftMotor() {
-		this.SPEED_LEFT = Math.min(this.SPEED_LEFT + this.PLUS_LEFT_DELTA, this.SPEED_MAX);
-		robot.setSpeed(this.SPEED_LEFT, this.SPEED_RIGHT);
-		robot.forward();
-	}
-	
-	private void speedUpRightMotor() {
-		this.SPEED_RIGHT = Math.min(this.SPEED_RIGHT + this.PLUS_RIGHT_DELTA, this.SPEED_MAX);
-		robot.setSpeed(this.SPEED_LEFT, this.SPEED_RIGHT);
-		robot.forward();
-	}
-	
 	private void slowDownLeftMotor() {
-		this.SPEED_LEFT = Math.max(this.SPEED_LEFT - this.MINUS_LEFT_DELTA, this.SPEED_MAX * 0.6f);
+//		this.turn_delta += this.TURN_INCREASE;
+		this.SPEED_LEFT = Math.max(this.SPEED_LEFT - this.MINUS_LEFT_DELTA - this.turn_delta, this.SPEED_MIN_LEFT);
 		robot.setSpeed(this.SPEED_LEFT, this.SPEED_RIGHT);
 		robot.forward();
 	}
 	
 	private void slowDownRightMotor() {
-		this.SPEED_RIGHT = Math.max(this.SPEED_RIGHT - this.MINUS_RIGHT_DELTA, this.SPEED_MAX * 0.6f);
+		this.turn_delta += this.TURN_INCREASE;
+		this.SPEED_RIGHT = Math.max(this.SPEED_RIGHT - this.MINUS_RIGHT_DELTA - this.turn_delta, this.SPEED_MIN_RIGHT);
 		robot.setSpeed(this.SPEED_LEFT, this.SPEED_RIGHT);
 		robot.forward();
 	}
